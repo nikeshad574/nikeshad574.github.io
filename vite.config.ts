8fd3f97699040bc1c8cbb3090d80f01fe2282821
Nikesh Adhikari <nikeshad574@gmail.com>
2026-02-06 11:22:51 +0545

    Root path fixed

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/nikeshad574.github.io/", // Replace with your actual repository name
+  base: "/", // Root path for username.github.io repository
   build: {
     outDir: "dist",
     sourcemap: false,
