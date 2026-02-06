6d986d9ed65a919064adf9662472b0a5bf37cf0f
Nikesh Adhikari <nikeshad574@gmail.com>
2026-02-06 11:21:30 +0545

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/nikeshad574.github.io/", // Replace with your actual repository name
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+  },
 });
